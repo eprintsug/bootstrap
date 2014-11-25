c8a33b2f4f2c6c6b22ecabc1b13236c1493c16e5
David McElroy <d.mcelroy@uel.ac.uk>
2014-11-25 17:39:54 +0000

    added forms.js and reworked workflow

---
+++ cfg/themes/bootstrap/static/bootstrap_assets/Scripts/forms.js
@@ -0,0 +1,29 @@
+/**************************
+ Script to add the bootstrap
+ form classes to the eprints
+ workflow
+***************************/
+var j = jQuery.noConflict();
+
+j(document).ready(function () {
+
+	// textarea stylings
+	j('.ep_sr_component textarea').addClass('form-control');	
+	
+	//name entry style stuff (work in progress...)
+	//j('.ep_form_input_grid input.ep_form_text').addClass('form-control');	
+	
+	//Select. Using the jquery selector here to only select dvisions as i'm worried about styling ALL selects in eprints
+	j('select[id$=divisions]').addClass('form-control');	
+	
+	// bootstrap radiobuttons
+	j('input:radio').parent().parent().addClass("radio")
+
+	j('select[id$=divisions]').addClass('form-control');	
+	
+	//j('.ep_form_field_input .ep_sr_component').addClass('panel panel-default').removeClass('ep_sr_compenent');
+
+	// Style the collapses bars to look okay
+	j('.ep_sr_component .ep_sr_collapse_bar').addClass('panel-heading').removeClass('ep_sr_collapse_bar');
+
+});
