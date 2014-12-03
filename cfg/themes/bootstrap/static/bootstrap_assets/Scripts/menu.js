2e05fd81209df1f3dc31a4370fd9605f0413d0f9
David McElroy <d.mcelroy@uel.ac.uk>
2014-12-03 11:52:33 +0000

    updated menu.js and template to clone menu to the navbar rather than call fromt he pin again. Pins in Phrases do not make for happy generate_statics.

--- cfg/themes/bootstrap/static/bootstrap_assets/Scripts/menu.js
+++ cfg/themes/bootstrap/static/bootstrap_assets/Scripts/menu.js
@@ -7,23 +7,21 @@ var j = jQuery.noConflict();
 
 j(document).ready(function () {
 
-	//left hand side menu. Addes the classes needed for bootstrap to style the menu 
-	j('#main_page ul.ep_tm_key_tools ').addClass('list-group');
-	j('#main_page ul.ep_tm_key_tools li').addClass('list-group-item');
-	j('#main_page ul.ep_tm_key_tools li:first-child').addClass('active');
-	j('#main_page ul.ep_tm_key_tools li:first-child span a').addClass('list-group-item-text');
-	j('#main_page ul.ep_tm_key_tools li:first-child a[href$="cgi/users/home"]').addClass('list-group-item-heading');
-
-	
-	
-	//menu manipluation and collapse stuff
-	//if on side menu {
-	j('#main_page .ep_tm_key_tools').removeClass('ep_tm_key_tools').addClass('navbar-collapse collapse').attr('id','ep_menu');
-	//}else {
+	//Clones the EP menu to the top menu. 
+	j('#main_page .ep_tm_key_tools').clone().appendTo('#login_status');
+	//Then remove the old class, and add the navbar classes to make it fit in. 
 	j('header .ep_tm_key_tools').removeClass('ep_tm_key_tools').addClass('nav navbar-nav navbar-right').attr('id','ep_menu_top');
-	//}
-	//j('nav #ep_menu li:not(:first-child)').wrapInner('<a style="cursor:default;color:#555 " href="#"></a>');
-	//j('nav #ep_menu li:first-child').has("span").wrapInner('<div class="well well-sm"></a>');
+	// remove the first part of the menu if you are already signed in (as the span buggers up the nice navbar menu)
 	j('header #ep_menu_top li:first-child').has("span").remove();
+	
+
+	//left hand side menu. Adds the id, then the needed classes for bootstrap to style the menu 
+	j('#main_page .ep_tm_key_tools').removeClass('ep_tm_key_tools').attr('id','ep_menu');
+	j('#main_page ul#ep_menu ').addClass('list-group');
+	j('#main_page ul#ep_menu li').addClass('list-group-item');
+	j('#main_page ul#ep_menu li:first-child').addClass('active');
+	j('#main_page ul#ep_menu li:first-child span a').addClass('list-group-item-text');
+	j('#main_page ul#ep_menu li:first-child a[href$="cgi/users/home"]').addClass('list-group-item-heading');
+
 
 });
