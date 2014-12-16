5febd6ceb68ae74ef0109a28b7e77670b6706355
David McElroy <d.mcelroy@uel.ac.uk>
2014-12-16 16:11:23 +0000

    tidied up and more targeted selectors to avoid breaking workflow

--- cfg/themes/bootstrap/static/bootstrap_assets/Scripts/admin_menus.js
+++ cfg/themes/bootstrap/static/bootstrap_assets/Scripts/admin_menus.js
@@ -7,13 +7,14 @@ var j = jQuery.noConflict();
 
 j(document).ready(function () {
 
-	//j('#ep_admin_tabs_panels dl').addClass('row').removeClass('ep_action_list');
-	j('dl').addClass('row').removeClass('ep_action_list').css('margin-top','1em');
-	//j('#ep_admin_tabs_panels dl dt').addClass('col-md-offset-0 col-md-4 col-sm-4 text-right ');
-	j('dl dt').addClass('col-md-offset-0 col-md-4 col-sm-4 text-right ');
-	//j('#ep_admin_tabs_panels dl dd').addClass('col-md-8 col-sm-8').css("min-height","3em");
-	j('dl dd').addClass('col-md-8 col-sm-8').css("min-height","3em");
-
-	
+	// The dl dt and dd that will be moved into columns
+	var ep_dls = j('#ep_admin_tabs_panels dl, #ep_eprint_view_panels dl');
+	var ep_dts = j('#ep_admin_tabs_panels dl dt, #ep_eprint_view_panels dl dt');
+	var ep_dds = j('#ep_admin_tabs_panels dl dd, #ep_eprint_view_panels dl dd');
+		
+	// add the columns
+	ep_dls.addClass('row').removeClass('ep_action_list').css('margin-top','1em');
+	ep_dts.addClass('col-md-offset-0 col-md-4 col-sm-4 col-xs-4 text-right ');
+	ep_dds.addClass('col-md-8 col-sm-8 col-xs-8').css("min-height","3em");
 
 });
