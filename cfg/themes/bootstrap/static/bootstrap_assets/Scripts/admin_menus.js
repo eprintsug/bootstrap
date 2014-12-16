eb724562c069fa60d5eb33299ceae5fc3644dd5a
David McElroy <d.mcelroy@uel.ac.uk>
2014-12-16 14:55:50 +0000

    might need to change the name of admin_menus.js as it now works for all dt dl lists.

--- cfg/themes/bootstrap/static/bootstrap_assets/Scripts/admin_menus.js
+++ cfg/themes/bootstrap/static/bootstrap_assets/Scripts/admin_menus.js
@@ -7,8 +7,13 @@ var j = jQuery.noConflict();
 
 j(document).ready(function () {
 
-	j('#ep_admin_tabs_panels dl').addClass('row').removeClass('ep_action_list');
-		j('#ep_admin_tabs_panels dl dt').addClass('col-md-offset-0 col-md-4 col-sm-4 text-right ');
-	j('#ep_admin_tabs_panels dl dd').addClass('col-md-8 col-sm-8').css("min-height","3em");
+	//j('#ep_admin_tabs_panels dl').addClass('row').removeClass('ep_action_list');
+	j('dl').addClass('row').removeClass('ep_action_list').css('margin-top','1em');
+	//j('#ep_admin_tabs_panels dl dt').addClass('col-md-offset-0 col-md-4 col-sm-4 text-right ');
+	j('dl dt').addClass('col-md-offset-0 col-md-4 col-sm-4 text-right ');
+	//j('#ep_admin_tabs_panels dl dd').addClass('col-md-8 col-sm-8').css("min-height","3em");
+	j('dl dd').addClass('col-md-8 col-sm-8').css("min-height","3em");
+
+	
 
 });
