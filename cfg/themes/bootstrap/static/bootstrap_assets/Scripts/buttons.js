6ff264c7212e6a9b52056461de8f32c5716846fa
David McElroy <d.mcelroy@uel.ac.uk>
2014-12-17 16:44:50 +0000

    trying ot AJAX issue solutions in button.js. Moved all class add stuff to a function

--- cfg/themes/bootstrap/static/bootstrap_assets/Scripts/buttons.js
+++ cfg/themes/bootstrap/static/bootstrap_assets/Scripts/buttons.js
@@ -7,9 +7,16 @@ var j = jQuery.noConflict();
 
 j(document).ready(function () {
 
-	var buttons_1 = j('.ep_action_list .ep_form_action_button, .ep_form_action_button, .ep_blister_node_selected, .ep_form_internal_button');
-	var buttons_2 = j('.ep_blister_node');
-	buttons_1.addClass('btn btn-primary').removeClass('ep_form_action_button');
-	buttons_2.addClass('btn btn-default').removeClass('ep_form_action_button');
+	updateButtons();
+
+	//j('a[href$="ep_tabs_current=Admin%3A%3AEPM%3A%3AAvailable"]').attr('onclick','').unbind('click');
 
 });
+
+function updateButtons() {
+
+	var buttons_1 = j('.ep_action_list .ep_form_action_button, .ep_form_action_button, .ep_blister_node_selected, .ep_form_internal_button');
+        var buttons_2 = j('.ep_blister_node');
+        buttons_1.addClass('btn btn-primary').removeClass('ep_form_action_button');
+        buttons_2.addClass('btn btn-default').removeClass('ep_form_action_button');
+}
