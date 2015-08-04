539cd966b28be10bfe921af90ed7bc363f63390e
Denix <denics@free.fr>
2015-08-04 12:19:50 +0200

    Update z_theme.pl
    Added plugin map to enable bootstrap login screen

--- cfg/cfg.d/z_theme.pl
+++ cfg/cfg.d/z_theme.pl
@@ -1,1 +1,5 @@
 $c->{theme} ="bootstrap";
+
+# To enable bootstrap login screen
+$c->{plugin_alias_map}->{"Screen::Login::Internal"} = "Screen::Login::BootstrapLogin";
+$c->{plugin_alias_map}->{"Screen::Login::BootstrapLogin"} = undef;
